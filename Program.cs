2dca3dccb3c6d1f43f532225c9c3ce6a7938c73a
gokdeniz <ayrilmis.gokdeniz@gmail.com>
2025-02-02 15:28:52 +0300

    pseudo kod yazildi.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,43 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //a = X karenin katsayısını giriniz.
+        // b = x değerinin katsayısını veriniz.
+        // c = sabit değeri giriniz.
+        // delta = (b^2) - 4 * a * c
+        // if(delta>0)
+        // {
+        //  kok1 = (-b + (d^1/2)) / 2a
+        //  kok2 = (-b - (d^1/2)) / 2a
+        // print kok1 , kok2
+        // }
+        //
+        // else if (delta == 0)
+        // {
+        //  kok = (-b) / 2a
+        //  print kokler esit
+        //  print kok
+        // }
+        //
+        //  else
+        // {
+        //  print reel kök yoktur.
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
